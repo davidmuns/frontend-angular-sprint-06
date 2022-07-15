818dc2b72573c40fa0b10574b6f2782719e60eec
David Gomez Muns <davidmuns@yahoo.es>
2022-07-15 23:53:42 +0200

    Update escena.component.ts

--- src/app/escena/escena.component.ts
+++ src/app/escena/escena.component.ts
@@ -9,9 +9,26 @@ import { IStory } from './../istory';
 })
 export class EscenaComponent implements OnInit {
   @Input('parentData') public stories: IStory[] = [];
+  private currentScene: number = 0;
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  prev() {
+    if (this.currentScene == 0)
+      this.currentScene = this.stories.length - 1;
+    else
+      this.currentScene--;
+  }
+
+  next() {
+    if (this.currentScene == this.stories.length - 1)
+      this.currentScene = 0;
+    else
+      this.currentScene++;
+  }
+
+  getCurrentScene = () => this.currentScene;
 
 }
