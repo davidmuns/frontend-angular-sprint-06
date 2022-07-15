90e30963dd38723f5f4be8c813359a280a87a9a9
David Gomez Muns <davidmuns@yahoo.es>
2022-07-15 19:52:29 +0200

    Update escena.component.ts

--- src/app/escena/escena.component.ts
+++ src/app/escena/escena.component.ts
@@ -1,8 +1,6 @@
-import { IStory } from './../istory';
 import { SelectorListContext } from '@angular/compiler';
 import { Component, Input, OnInit } from '@angular/core';
-import storiesJson from '../stories.json'
-
+import { IStory } from './../istory';
 
 @Component({
   selector: 'app-escena',
@@ -10,7 +8,7 @@ import storiesJson from '../stories.json'
   styleUrls: ['./escena.component.css']
 })
 export class EscenaComponent implements OnInit {
-  public stories: IStory[] = storiesJson;
+  @Input('parentData') public stories: IStory[] = [];
   constructor() { }
 
   ngOnInit(): void {
