2a13f964cb39de66c0b614b75f712f9cd0ca8628
David Gomez Muns <davidmuns@yahoo.es>
2022-07-16 15:08:35 +0200

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `<app-home></app-home>`
 })
 export class AppComponent {
   title = 'frontend-angular-sprint-06';
